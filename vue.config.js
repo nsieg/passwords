354cb7d6fed6f934efae8a0b1f538cdf6483968a
nsieg <n.siegfried@gmx.de>
2021-05-14 14:47:11 +0200

    ci

--- vue.config.js
+++ vue.config.js
@@ -15,6 +15,13 @@ module.exports = {
         themeColor: "#00d1b2",
         msTileColor: "#00d1b2",
         appleMobileWebAppCapable: "yes",
-        appleMobileWebAppStatusBarStyle: "default"
+        appleMobileWebAppStatusBarStyle: "default",
+        manifestOptions: {
+            name: pwa.name,
+            short_name: pwa.name,
+            start_url: '/?utm_source=homescreen',
+            display: 'standalone',
+            theme_color: pwa.themeColor
+        }
     }
 };
