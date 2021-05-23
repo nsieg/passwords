b07a0c77b5c8403858e692b181cc6162f6464328
nsieg <n.siegfried@gmx.de>
2021-05-23 11:18:51 +0200

    Updated dependencies

--- src/components/Tour.vue
+++ src/components/Tour.vue
@@ -6,14 +6,12 @@
 
 <script>
 import "intro.js/minified/introjs.min.css";
-//import "intro.js/themes/introjs-modern.css";
 import introJs from 'intro.js';
 
 export default {
   name: 'Tour',
   methods: {
     startTour () {
-      console.log(introJs)
         introJs().setOptions({ 
           showBullets: false,
           hidePrev: true,
